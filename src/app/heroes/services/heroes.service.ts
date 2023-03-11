11623d507e5ceba436f4e58c94f84cd4fedb6f08
Jhon Acevedo <jhon.acevedo@inglesparatodos.edu.co>
2023-03-11 11:19:47 -0500

    CRUD create read update

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -18,7 +18,7 @@ export class HeroesService {
     return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes`);
   }
 
-  getHeroePorId( id: string ):Observable<Heroe> {
+  getHeroePorId( id: string ): Observable<Heroe> {
     return this.http.get<Heroe>(`${ this.baseUrl }/heroes/${ id }`);
   }
 
@@ -26,4 +26,11 @@ export class HeroesService {
     return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes?q=${ termino }&_limit=6`);
   }
 
+  agregarHeroe( heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${ this.baseUrl }/heroes`, heroe );
+  }
+
+  actualizarHeroe( heroe: Heroe): Observable<Heroe> {
+    return this.http.put<Heroe>(`${ this.baseUrl }/heroes/${ heroe.id }`, heroe );
+  }
 }
