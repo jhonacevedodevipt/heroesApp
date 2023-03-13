e2309d26e20957bcf6b259b3b6adfbfa2d50b22a
Jhon Acevedo <jhon.acevedo@inglesparatodos.edu.co>
2023-03-13 17:09:03 -0500

    CRUD | Angular Guards | Can Load and Can Activate

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,18 @@ import { Router } from '@angular/router';
   styles: [],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private authService: AuthService) {}
 
   login() {
     // Ir al backend
     // un usuario
-    this.router.navigate(['./heroes']);
+
+    this.authService.login()
+      .subscribe( resp => {
+        console.log(resp);
+        this.router.navigate(['./heroes']);
+      });
+
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +14,17 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( private router: Router ) { }
+  get auth() {
+    return this.authService.auth;
+  }
+
+  constructor( private router: Router,
+               private authService: AuthService ) { }
 
   ngOnInit(): void {
   }
 
   logout() {
-    this.router.navigate(['./auth'])
+    this.router.navigate(['./auth']);
   }
 }
